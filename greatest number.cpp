8f191e7babe871a7f207839c4371e474a13549c6
kowsigashri <kowsigashri2006@gmail.com>
2024-09-18 23:34:29 +0530

    Add files via upload

---
+++ greatest number.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+     int a,b;
+     printf("Enter two numbers:");
+     scanf("%d %d",&a,&b);
+     if(a>b)
+            printf("a is biggest number\n");
+     else
+            printf("b is biggest rumber\n");
+     return 0;
+}
+                 
+                 
