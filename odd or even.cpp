6f104fe5690247677f8a751c4526a9d5e0aace56
kowsigashri <kowsigashri2006@gmail.com>
2024-09-18 23:34:11 +0530

    Add files via upload

---
+++ odd or even.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    if ((num / 2) * 2 == num) 
+        printf("%d is Even\n",a);
+    else 
+        printf("%d is Odd\n",a);
+    return 0;
+}
+
+
+
